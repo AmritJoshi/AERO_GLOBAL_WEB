df868a830bf4af4edd8126c9ef32367bccdf2fbd
Amrit Joshi <amritscience2012@gmail.com>
2024-12-22 22:10:08 +0545

    adding team

--- src/components/AboutUs.components.jsx
+++ src/components/AboutUs.components.jsx
@@ -7,7 +7,10 @@ import Box from './Box.components.jsx';
 import LapRead from '../utils/ReadLap.jpg';
 import Cop from '../utils/Cop.jpeg';
 import University from '../utils/University.jpg';
-
+import SumanK from '../utils/TeamImages/SumanK.jpeg';
+import SumanS from '../utils/TeamImages/SumanS.jpeg';
+import YogeshJ from '../utils/TeamImages/YogeshJ.jpeg';
+import SamirT from '../utils/TeamImages/SamirT.jpeg';
 const AboutUs=()=>{
   return (
     <div>
@@ -17,6 +20,33 @@ const AboutUs=()=>{
       <p>Aero Education Consultancy is a premier educational advisory firm dedicated to guiding students towards their academic dreams and career aspirations. With a deep understanding of the global education landscape, we specialize in providing tailored guidance for students seeking higher education opportunities abroad. Our team of experienced consultants offers personalized support in choosing the right programs, universities, and countries that align with each student’s goals and interests. At Aero Education Consultancy, we are committed to excellence, integrity, and the success of our clients, ensuring a smooth and informed journey from application to admission and beyond. Our mission is to empower students to reach new heights in their educational and professional endeavors through expert advice and unwavering support.
       </p>
       </div>
+      </div>
+      <div>
+        <div className="Team">
+        <h1>Our Team</h1>
+        </div>
+        <div className="portfolio-grid">
+        <div className="CEO_DES">
+        <img src={SumanK} alt="CEO" className='CEO'/>
+          <h3 style={{marginTop:"5px"}}>Suman Kunwar</h3>
+          <h3>CEO</h3>
+        </div>
+        <div className="SConsultant_DES">
+          <img src={YogeshJ} alt="SConsultant" className="SConsultant" />
+          <h3 style={{marginTop:"5px"}}>Yogesh Joshi</h3>
+          <h3>Senior Consultant</h3>
+        </div>
+        <div className="COO_DES">
+          <img src={SumanS} alt="COO" className="COO"/>
+          <h3 style={{marginTop:"5px"}}>Suman Shrestha</h3>
+          <h3>COO</h3>
+        </div>
+        <div className="AdHead_DES">
+          <img src={SamirT} alt="AdHead" className="AdHead"/>
+          <h3 style={{marginTop:"5px"}}>Samir Thakuri</h3>
+          <h3>Administrative Head/Consultant</h3>
+        </div>
+        </div>
       </div>
       <div className='Aim'>
         <h1>Our Aim and Vison</h1>
