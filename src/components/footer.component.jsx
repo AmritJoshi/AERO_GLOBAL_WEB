d17aa87d4fa25f82390f83de9c5db2bc123fd9c0
Amrit Joshi <amritscience2012@gmail.com>
2024-12-31 15:58:51 +0545

    new social added

--- src/components/footer.component.jsx
+++ src/components/footer.component.jsx
@@ -8,8 +8,8 @@ const Footer = () =>{
   <div>
     
   <div className="SocialMedia">
-  <a href='http://www.instagram.com/aeroglobalconsultancy/' rel="noopener noreferrer" target='_blank'><FontAwesomeIcon icon={faSquareInstagram} style={{width:'40px',height:'40px'}}/></a>
-  <a href='https://www.facebook.com/profile.php?id=61562753673046' rel="noopener noreferrer" target='_blank'><FontAwesomeIcon icon={faSquareFacebook} style={{width:'40px',height:'40px',marginLeft:'4px'}} /></a>
+  <a href='https://www.instagram.com/aero_global_consultancy_nepal/profilecard/?igsh=ejMxaGQ3cHgwcnI5' rel="noopener noreferrer" target='_blank'><FontAwesomeIcon icon={faSquareInstagram} style={{width:'40px',height:'40px'}}/></a>
+  <a href='https://www.facebook.com/profile.php?id=61565007855319' rel="noopener noreferrer" target='_blank'><FontAwesomeIcon icon={faSquareFacebook} style={{width:'40px',height:'40px',marginLeft:'4px'}} /></a>
   <a href="https://np.linkedin.com/in/aero-global-consultancy-20a13531a"  rel="noopener noreferrer" target='_blank'><FontAwesomeIcon icon={faLinkedin} style={{width:'40px',height:'40px',marginLeft:'4px'}} /></a>
   <FontAwesomeIcon icon={faYoutube} style={{width:'40px',height:'40px',marginLeft:'4px'}} />
   </div>
